35f16dd6d7e8a3ffeded821e9c9ff714026f183f
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-11-28 01:56:02 +0300

    create class Picturs and make migrations

--- gallery/models.py
+++ gallery/models.py
@@ -2,5 +2,8 @@ from django.db import models
 from django.db.models.fields import CharField
 
 # Create your models here.
-
-
+class Picture(models.Model):
+    image_name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to = 'images/')
+    image_discription = models.CharField(max_length=60)
+    
